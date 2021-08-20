be6c842351f170d1f0e7c1535cfe60a2f00260dc
Thiago Sotto <thiago.sotto@hotmail.com>
2021-08-20 10:10:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# app_help_desk
+ Projeto de conclusão de curso de desenvolvimento Web completo 2021. Do módulo de PHP, com um aplicativo de HELP-DESK do professor Jorge Sant Ana
---
+++ valida_login.php
@@ -0,0 +1,21 @@
+<?php
+
+    /* super global get com essa as informações de email e senha aparecem no browse
+    print_r($_GET);
+
+    echo '<br />';
+
+    echo $_GET['email'];
+    echo '<br />';
+    echo $_GET['senha'];*/
+
+    print_r($_POST);
+
+    echo '<br />';
+
+    echo $_POST['email'];
+    echo '<br />';
+    echo $_POST['senha'];
+
+
+?>
