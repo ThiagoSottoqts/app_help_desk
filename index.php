a48adf08a6be52f7a180f6733bdded0a91551f46
Thiago Sotto <thiago.sotto@hotmail.com>
2021-08-20 11:22:35 -0300

    Autenticação do usuário e mensagem de erro ao usuário

--- index.php
+++ index.php
@@ -39,6 +39,17 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+                <?php
+                    // essa função verifica se um determinado indice esta citado.
+                    if(isset($_GET['login']) && $_GET['login'] == 'erro') {
+                     
+                     ?>
+                          <div class="text-danger d-flex justify-content-center ">
+                            Usuário ou senha inválidos
+                          </div>
+
+                     <?php } ?>     
+                    
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- valida_login.php
+++ valida_login.php
@@ -1,6 +1,42 @@
 <?php
 
-    /* super global get com essa as informações de email e senha aparecem no browse
+    //variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false; //
+
+    //usuários do sistema
+    $usuarios_app = [
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    ];
+
+    /* para verificar se o array estava funcionando
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '<pre />';*/
+
+    foreach($usuarios_app as $user) {  //forech percorre cada um dos elementos e os as nos da acesso a cada um dos valores dos arrays de forma individual
+       if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+       }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');// essa função força o redirecionamento.
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /* super global get com essa as informações de email e senha aparecem no browse - capturar dados do formulário de acesso
     print_r($_GET);
 
     echo '<br />';
